466f6b0f06560415a93f1808557b5ad24a597bfd
Taniadfs <t.dangelofonfria@gmail.com>
2025-06-03 06:38:52 +0200

    Creacion nav y inicio funcion

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
 import './style.css'
-docmunt.querySelector('#header').innerHTML = `
+
+const header = document.querySelector('header')
+const nav = document.createElement('nav')
+nav.classList.add('nav')
+header.appendChild(nav)
+
+const navleft = crearNavLeft()
+const navSearch = crearNavSearch()
+const navright = crearNavRight()
+
+nav.append(navleft, navSearch, navright)
+
+function crearNavLeft ()
