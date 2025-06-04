dbcbf843ec2caa98045185b37b96c57a5a4539c1
Taniadfs <t.dangelofonfria@gmail.com>
2025-06-04 12:48:32 +0200

    funciones nav

--- src/main.js
+++ src/main.js
@@ -11,4 +11,24 @@ const navright = crearNavRight()
 
 nav.append(navleft, navSearch, navright)
 
-function crearNavLeft ()
+function crearNavLeft() {
+  const left = document.createElement('div')
+  left.classList.add('nav-left')
+
+  const logo = document.createElement('img')
+  logo.src = '/public/Assets/logo-pinterest-vectoriel.svg'
+  logo.alt = 'Logo de Pinterest'
+  logo.classList.add('logo')
+  left.appendChild(logo)
+
+  const botones = ['Incio', 'Explorar', 'Crear']
+  botones.forEach((boton) => {
+    const btn = document.createElement('button')
+    btn.textContent = boton
+    btn.classList.add('nav-button')
+    left.appendChild(btn)
+  })
+  return left
+}
+
+console.log(crearNavLeft())
