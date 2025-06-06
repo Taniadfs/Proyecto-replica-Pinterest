88861ce300696828b6caa44c448820bcc8ab3be3
Taniadfs <t.dangelofonfria@gmail.com>
2025-06-06 09:58:00 +0200

    stylos css nav left y nav search

--- src/main.js
+++ src/main.js
@@ -34,6 +34,9 @@ function crearNavSearch() {
   const navSearch = document.createElement('div')
   navSearch.classList.add('nav-search')
 
+  const wrapperSearch = document.createElement('div')
+  wrapperSearch.classList.add('search-wrapper')
+
   const inputSearch = document.createElement('input')
   inputSearch.type = 'text'
   inputSearch.placeholder = 'Buscar en Inspirest'
@@ -44,7 +47,9 @@ function crearNavSearch() {
   searchButton.src = '/Assets/buscar.png'
   searchButton.alt = 'Buscar'
 
-  navSearch.append(inputSearch, searchButton)
+  wrapperSearch.append(searchButton, inputSearch)
+  navSearch.append(wrapperSearch)
+
   return navSearch
 }
 
